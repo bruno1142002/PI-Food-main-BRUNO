08774c034e9e5b49784f262e72720b879d8a9f40
Bruno Sentinelli <brunosentinelli@gmail.com>
2021-09-28 13:47:20 -0300

    CAMBIOS ROUTE

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -1,14 +1,14 @@
 const {Router}= require('express');
 const {Type} = require('../db');
 const { default: axios } = require('axios');
-const {YOUR_API_KEY}= process.env;
+const {YOUR_API_KEY}= process.env; //ARREGLAR API//
 const{URL_RECIPE, RECIPE_INFO}= require('../utils/path');
 
 const route = Router();
 
 route.get('/', async(req, res)=>{
-//     Obtener todos los tipos de dieta posibles
-// En una primera instancia, cuando no exista ninguno, deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá(link)
+//     Aparece todos los tipos de dieta posible
+// Si  no existe ninguno, deberán precargar la base de datos con los tipos de datos indicados por spoonacular (API)
 let allRecipes = await Type.findAll();
 	return res.send(allRecipes);
 })
